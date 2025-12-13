99657e2c95c4afc17262b4f581c8ecdbfdb0d3d3
Sir-Teejay <113782092+Sir-Teejay@users.noreply.github.com>
2025-12-13 16:36:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('/webhook', (req, res) => {
   const token = req.query['hub.verify_token'];
   const challenge = req.query['hub.challenge'];
 
-  if (token === verifyToken) {
+  if (mode === 'subscribe' && token === verifyToken) {
     console.log('Webhook verified successfully');
     return res.status(200).send(challenge);
   } else {
